aabb29c719bb10b1859d23f7566c26e3fcde1110
Celine Adam <celine.adaam@gmail.com>
2023-02-01 19:22:11 +0100

    update

--- src/index.js
+++ src/index.js
@@ -1,27 +1,28 @@
-import "./css/style.css"; //importing css
+// import "./css/style.css"; //importing css
 
-const dropdown = document.querySelector(`#dropdown`);
-const value = dropdown.options[dropdown.selectedIndex];
+const cryptoSelect = document.querySelector(".cryptoSelect");
 
 const inputAmount = document.querySelector(`#inputAmount`);
-const dropdown2 = document.querySelector(`#dropdown2`);
-const value2 = dropdown2.options[dropdown.selectedIndex];
+const fiatCurrencySelect = document.querySelector(".fiatCurrencySelect");
 
 const output = document.querySelector(`#output`);
 const form = document.querySelector("#conversion-form");
 
-async function fetchCryptocurrencies(e) {
-  e.preventDefault();
+async function fetchCryptocurrencies() {
   try {
     let response = await fetch(
-      `https://api.coinconvert.net/convert/${value.value}/${value2.value}?amount=${inputAmount.value}`
+      `https://api.coinconvert.net/convert/${cryptoSelect.value}/${fiatCurrencySelect.value}?amount=${inputAmount.value}`
     );
 
     let date = await response.json();
     console.log(date);
-    output.innerText = `${date.value2.value}`;
+    output.value = date[fiatCurrencySelect.value.toUpperCase()];
   } catch (err) {
     console.log(err);
   }
 }
-form.addEventListener("submit", fetchCryptocurrencies);
+fetchCryptocurrencies();
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  fetchCryptocurrencies();
+});
